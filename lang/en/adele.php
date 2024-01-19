0282a313806b392e02f2d7352db258e3d230bd30
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-01-19 10:55:47 +0100

    Init commit

---
+++ lang/en/adele.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_adele
+ * @category    string
+ * @copyright   2024 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Adele';
+$string['modulenameplural'] = 'adeles';
+$string['modulename'] = 'adele';
+$string['adelename'] = 'Adele';
+$string['adelename_help'] = 'Help for Adele';
+$string['adelefieldset'] = 'Adele Fieldset';
+$string['adelesettings'] = 'Adele Settings';
+$string['pluginadministration'] = 'Adele Plugin Administration';
+
+// Capabilities.
+$string['adele:addlearningpath'] = 'Can add learning paths';
+$string['mod/adele:seelearningpath'] = 'Can see learning paths';
---
+++ view.php
@@ -0,0 +1,82 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Prints an instance of mod_adele.
+ *
+ * @package     mod_adele
+ * @copyright   2024 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require(__DIR__.'/../../config.php');
+require_once(__DIR__.'/lib.php');
+
+// Course module id.
+$id = optional_param('id', 0, PARAM_INT);
+
+// Activity instance id.
+$a = optional_param('a', 0, PARAM_INT);
+
+if ($id) {
+    $cm = get_coursemodule_from_id('adele', $id, 0, false, MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $moduleinstance = $DB->get_record('adele', array('id' => $cm->instance), '*', MUST_EXIST);
+} else {
+    $moduleinstance = $DB->get_record('adele', array('id' => $a), '*', MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_instance('adele', $moduleinstance->id, $course->id, false, MUST_EXIST);
+}
+
+require_login($course, true, $cm);
+
+$modulecontext = context_module::instance($cm->id);
+
+$event = \mod_adele\event\course_module_viewed::create(array(
+    'objectid' => $moduleinstance->id,
+    'context' => $modulecontext
+));
+$event->add_record_snapshot('course', $course);
+$event->add_record_snapshot('adele', $moduleinstance);
+$event->trigger();
+
+$PAGE->set_url('/mod/adele/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($moduleinstance->name));
+$PAGE->set_heading(format_string($course->fullname));
+$PAGE->set_context($modulecontext);
+
+echo $OUTPUT->header();
+
+// Early bail out conditions.
+if (isloggedin() && !isguestuser()
+  && has_capability('mod/adele:addlearningpath', context_system::instance())) {
+    echo <<<'EOT'
+    <div> HERE we are Teacher!!!!!!</div>
+    <div id="teacherapp"></div>
+    EOT;
+}
+
+// Early bail out conditions.
+if (isloggedin() && !isguestuser()
+  && has_capability('mod/adele:seelearningpath', context_system::instance())
+  && !has_capability('mod/adele:addlearningpath', context_system::instance())) {
+    echo <<<'EOT'
+    <div> HERE we are Student Mudent!!!!!!</div>
+    <div id="studentapp"></div>
+    EOT;
+}
+
+echo $OUTPUT->footer();
