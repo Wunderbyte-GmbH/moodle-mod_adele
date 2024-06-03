0882be309607ad936ad4527c0976f696cb7c7061
cbadusch <hello@christianbadusch.codes>
2024-06-03 16:04:26 +0200

    Require local_adele

--- version.php
+++ version.php
@@ -26,6 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_adele';
 $plugin->release = '0.1.0';
-$plugin->version = 2024022100;
+$plugin->version = 2024060300;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
+$plugin->dependencies = [
+    'local_adele' => 2024060300,
+];
