aff0c9ac06f5d4ac963aac6214af2619df7248b5
Georg Maißer <georg.maisser@wunderbyte.at>
2025-01-29 15:47:32 +0100

    Tests: Add test generator

---
+++ tests/generator/lib.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class local_adele_generator for generation of dummy data
+ *
+ * @package local_adele
+ * @category test
+ * @copyright 2023 Andrii Semenets
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_adele_generator extends testing_module_generator {
+
+    /**
+     * Create booking instance
+     *
+     * @param mixed|null $record
+     * @param array|null $options
+     *
+     * @return stdClass
+     *
+     */
+    public function create_instance($record = null, ?array $options = null) {
+
+        $record['participantslist'] = [];
+
+        return parent::create_instance($record, $options);
+    }
+}
