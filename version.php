3eac7ef9d963a42457128e69b9c81fb8c32479dc
ralferlebach <ralf.erlebach@gmx.de>
2026-07-23 19:25:09 +0200

    v 0.1.9

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_adele';
 $plugin->supported = [405, 502];
-$plugin->release = '0.1.8';
-$plugin->version = 2026072302;
+$plugin->release = '0.1.9';
+$plugin->version = 2026072303;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
